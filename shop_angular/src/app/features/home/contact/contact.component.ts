40764f8962d3d05ad1178492322d3838957bdfe5
prabhjot <prabhjotsingh@usatechnologyllc.com>
2019-02-12 16:57:59 +0530

    terms and condition changes done

--- shop_angular/src/app/features/home/contact/contact.component.ts
+++ shop_angular/src/app/features/home/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-contact',
@@ -8,15 +9,75 @@ import { Router } from "@angular/router";
   styleUrls: ['./../home.component.css']
 })
 export class ContactComponent implements OnInit {
-  constructor(private router: Router) { }
+  validatorOptions = {
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      name: {
+        group: '.col-md-6',
+        validators: {
+          notEmpty: {
+            message: 'The name is required'
+          },
+          stringLength: {
+            max: 50,
+            message: 'The name must be less than 50 characters long'
+          }
+        }
+      },
+      email: {
+        group: '.col-md-6',
+        validators: {
+          notEmpty: {
+            message: 'The email is required'
+          },
+          emailAddress: {
+            message: 'The value is not a valid email address'
+          }
+        }
+      },
+      message: {
+        group: '.col-md-12',
+        validators: {
+          notEmpty: {
+            message: 'The message is required'
+          },
+          stringLength: {
+            max: 200,
+            message: 'The message must be less than 200 characters long'
+          }
+        }
+      },
+      subject: {
+        group: '.col-md-12',
+        validators: {
+          notEmpty: {
+            message: 'The subject is required'
+          },
+          stringLength: {
+            max: 80,
+            message: 'The subject must be less than 80 characters long'
+          }
+        }
+      },
+    }
+  };
+
+  constructor(private router: Router,private _flashMessagesService: FlashMessagesService) { }
 
   ngOnInit() { }
 
-  save(event) {
-    //console.log(event);
-    event.preventDefault();
-    localStorage.setItem('logged', 'yes');
-    this.router.navigate(['/home/dashboard']);
+  save(form) {
+    if (!form.invalid) {
+      const data = form.value;
+      console.log(data);
+      console.log('submitted');
+      form.reset();
+      this._flashMessagesService.show('Thanks for contact us!', { cssClass: 'alert-success', timeout: 2000 });
+    }
   }
 
 }
--- shop_angular/src/app/features/home/home.module.ts
+++ shop_angular/src/app/features/home/home.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { routing } from "./home.routing";
 import { HomeComponent } from './home.component';
 import { ContactComponent } from './contact/contact.component';
+import { SmartadminValidationModule } from '@app/shared/forms/validation/smartadmin-validation.module';
 
 @NgModule({
   imports: [
     CommonModule,
     routing,
+    FormsModule,
+    SmartadminValidationModule,
+    FlashMessagesModule.forRoot(),
+   
   ],
   declarations: [HomeComponent, ContactComponent],
   exports: []
